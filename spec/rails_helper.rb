8f9eadc1a6639711be73be89b32524619fd499ef
Kalina Nazi <knazi@uccs.edu>
2021-12-02 01:32:09 +0000

    FactoryBot Complete Ish

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,4 +77,8 @@ SimpleCov.start 'rails' do
   add_filter '/bin/'
   add_filter '/db/'
   add_filter '/spec/' # for rspec
+  filters.clear# This will remove the :root_filter and :bundler_filter that come via simplecov's defaults
+  add_filter do |src|
+  !(src.filename =~ /^#{SimpleCov.root}/) unless src.filename =~ /my_engine/
+end
 end
